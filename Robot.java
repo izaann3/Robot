1392dfe5e93613e96eb1f5a05542bad6c7beedba
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-05-24 08:31:36 +0200

    defino a los layouts de los cojones

--- Robot.java
+++ Robot.java
@@ -6,7 +6,14 @@ import java.awt.event.*;
 
 public class Robot extends JPanel {
     public Robot() {
-
+        setLayout(new GridLayout(4, 2));
+  
+        add(createButton("Moure Endavant"));
+        add(createButton("Girar Esquerra"));
+        add(createButton("Moure Enrere"));
+        add(createButton("Girar Dreta"))
+        add(createButton("Guardar Estat"));
+        add(createButton("Carregar Estat"));
     }
     
     private JButton createButton(String text) {
