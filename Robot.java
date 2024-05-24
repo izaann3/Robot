53900171f66a65f857132ee8fff184ef61b5aef1
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-05-24 09:48:01 +0200

    elimino el codigo y vuelvo a empezar, ahora ya lo puedo mover

--- Robot.java
+++ Robot.java
@@ -2,36 +2,36 @@ package Robot;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
-public class Robot extends JPanel {
-   public Robot() {
-       setLayout(new GridLayout(4, 2));
-  
-       add(createButton("Moure Endavant"));
-       add(createButton("Girar Esquerra"));
-       add(createButton("Moure Enrere"));
-       add(createButton("Girar Dreta"));
-       add(createButton("Guardar Estat"));
-       add(createButton("Carregar Estat"));
-   }
-  
-   private JButton createButton(String text) {
-       JButton button = new JButton(text);
-       button.setPreferredSize(new Dimension(50, 30));
-       return button;
-   }
-  
-   public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setTitle("Control de Robot");
-        frame.setResizable(false);
-        frame.add(new Robot());
-        frame.setLocationRelativeTo(null);
-        frame.setSize(400, 300);
+public class Robot {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Control de robot");
+        JTextField pantalla = new JTextField();
+        JPanel buttonsPanel = new JPanel();
+
+        JButton endavant = new JButton("Moure Endavant");
+        JButton enrere = new JButton("Moure Enrere");
+        JButton esquerra = new JButton("Girar Esquerra");
+        JButton dreta = new JButton("Girar Dreta");
+        JButton guardar = new JButton("Guardar Estat");
+        JButton carregar = new JButton("Carregar Estat");
+
+        pantalla.setEditable(false);
+        
+        buttonsPanel.add(pantalla);
+        buttonsPanel.add(endavant);
+        buttonsPanel.add(enrere);
+        buttonsPanel.add(esquerra);
+        buttonsPanel.add(dreta);
+        buttonsPanel.add(guardar);
+        buttonsPanel.add(carregar);
+
+        frame.add(buttonsPanel);
+      
+        frame.setSize(375, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-   }
+    }
 }
 
 
